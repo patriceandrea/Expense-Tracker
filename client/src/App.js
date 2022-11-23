d853a4d36bc2a5168b43f2d565855ef1bec341a7
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-23 17:02:15 -0500

    adds components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
     </div>
   );
 }
